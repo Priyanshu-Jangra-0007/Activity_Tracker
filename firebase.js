e89c7e1e59361f9cd1f45009e757f1ecffeb1bcf
Priyanshu-Jangra-0007 <priyanshujangra145@gmail.com>
2026-01-01 18:54:33 +0530

    Integrated Firebase authentication with route protection

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// firebase.js — PURE JS FILE
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCuk1-E7-RfE7coqA9hh4rAFpTNIlyTbV8",
+  authDomain: "activity-tracker-web-dc8bb.firebaseapp.com",
+  projectId: "activity-tracker-web-dc8bb",
+  storageBucket: "activity-tracker-web-dc8bb.firebasestorage.app",
+  messagingSenderId: "243753651024",
+  appId: "1:243753651024:web:3030467037a6c830b63a55"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
